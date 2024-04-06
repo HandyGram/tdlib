9a591042a46af359ea64f6724cf70f352a271428
Roman Rihter <teledurak@gmail.com>
2024-04-06 10:17:16 +0300

    update-tdlib: update README and commit automatically

--- update-tdlib.sh
+++ update-tdlib.sh
@@ -31,13 +31,25 @@ NEW_PATCH_LEVEL=$((OLD_PATCH_LEVEL+1))
 NEW_VERSION="$(cut -d '.' -f 1 <<< $OLD_VERSION).$(cut -d '.' -f 2 <<< $OLD_VERSION).$NEW_PATCH_LEVEL"
 # not sed -i cuz for compatibility between darwin and linux sed
 sed 's/version: '$(sed 's/\./\\\./g' <<< "$OLD_VERSION")'/version: '$(sed 's/\./\\\./g' <<< "$NEW_VERSION")'/' "$PACKAGE_DIR/pubspec.yaml" > "$PACKAGE_DIR/pubspec.yaml.tmp" || exit 1
+sed -r 's/v[0-9]+.[0-9]+.[0-9]+/v'$TDLIB_VERSION'/' "$PACKAGE_DIR/README.md" > "$PACKAGE_DIR/README.md.tmp" || exit 1
 cat - "$PACKAGE_DIR/CHANGELOG.md" <<< "## $NEW_VERSION
 * Updated to TDLib $TDLIB_VERSION ($TDLIB_COMMIT)
 " > "$PACKAGE_DIR/CHANGELOG.md.tmp" || exit 1
 mv "$PACKAGE_DIR/pubspec.yaml.tmp" "$PACKAGE_DIR/pubspec.yaml"
 mv "$PACKAGE_DIR/CHANGELOG.md.tmp" "$PACKAGE_DIR/CHANGELOG.md"
+mv "$PACKAGE_DIR/README.md.tmp" "$PACKAGE_DIR/README.md"
 
 cd "$PACKAGE_DIR"
 rm -rf "$TEMP_DIR"
 dart run generator/generate.dart || exit 1
+
+git add \
+    pubspec.yaml \
+    CHANGELOG.md \
+    README.md \
+    lib/src/tdapi \
+    data \
+    android/src/main/jniLibs
+git commit -m "Update to TDLib $TDLIB_VERSION ($TDLIB_COMMIT)"
+
 echo "Done."
